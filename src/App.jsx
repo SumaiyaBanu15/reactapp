f2c12bfb1023b9bea53efcded04b821adfad028d
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2024-01-09 11:16:14 +0530

    code_updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ function App() {
   const [count, setCount] = useState(0);
 
   const handleIncrement = () => {
-    setCount(count +1);
+    setCount(count + 1);
+  }
+  const handleDecrement = () => {
+    setCount(count - 1);
   }
 return (
    <div>
-    <h1>Increment Function</h1>
+    <h1>Count Function</h1>
     <div className="increment">
       <button onClick={handleIncrement}> + </button> &nbsp;&nbsp;&nbsp;
-      <span> {count} </span>
+      <span> {count} </span> &nbsp;&nbsp;&nbsp;
+      <button onClick={handleDecrement}> - </button>
     </div>
    </div>
 )
